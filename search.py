00f1f525e5bb39457c530000af56414a23ad41e5
Zaid Khan <brain@zaidkhan.me>
2022-12-09 05:06:36 +0000

    added caching

--- search.py
+++ search.py
@@ -7,6 +7,7 @@ import time
 import datetime
 import json
 from pathlib import Path
+from typing import List
 
 import torch
 import torch.nn as nn
@@ -73,22 +74,35 @@ def search_with_simla(
     text_atts = torch.cat(text_atts, dim=0)
     text_tokens = torch.cat([_.input_ids for _ in text_inputs], dim=0)
 
-    image_feats = []
-    image_embeds = []
-    for image, img_id in tqdm(data_loader):
-        image = image.to(device)
-        image_feat = model.visual_encoder(image)
-        image_embed = model.vision_proj(image_feat[:, 0, :])
-        image_embed = F.normalize(image_embed, dim=-1)
 
-        image_feats.append(image_feat)
-        image_embeds.append(image_embed)
+    cache = Path(config["cache"])
+    cache.mkdir(parents=True, exist_ok=True)
 
-    image_feats = torch.cat(image_feats, dim=0)
-    image_embeds = torch.cat(image_embeds, dim=0)
+    if (cache / "image_feats.pt").exists() and (cache / "image_embeds.pt").exists():
+        print("Loading image features from cache...")
+        image_feats = torch.load(cache / "image_feats.pt")
+        image_embeds = torch.load(cache / "image_embeds.pt")
+    else:
+        print("Cache not found. Computing image features...")
+        image_feats = []
+        image_embeds = []
+        for image, img_id in tqdm(data_loader):
+            image = image.to(device)
+            image_feat = model.visual_encoder(image)
+            image_embed = model.vision_proj(image_feat[:, 0, :])
+            image_embed = F.normalize(image_embed, dim=-1)
+
+            image_feats.append(image_feat)
+            image_embeds.append(image_embed)
+
+        image_feats = torch.cat(image_feats, dim=0)
+        image_embeds = torch.cat(image_embeds, dim=0)
+
+        torch.save(image_feats, cache / "image_feats.pt")
+        torch.save(image_embeds, cache / "image_embeds.pt")
+        print('Cached image features at {}'.format(cache))
 
     sims_matrix = image_embeds @ text_embeds.t()
-    # sims_matrix = torch.Tensor(np.load('/net/acadia10a/data/zkhan/albef-sims/albef-epoch30-sims.npy')).to(device)
 
     num_tasks = utils.get_world_size()
     rank = utils.get_rank()
@@ -137,189 +151,6 @@ def search_with_simla(
     import ipdb; ipdb.set_trace()
 
     return score_matrix_t2i.cpu().numpy()
-
-@torch.no_grad()
-def evaluation(model, data_loader, tokenizer, device, config):
-    raise ValueError("Do not call this function.")
-    # test
-    model.eval()
-
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    header = "Evaluation:"
-
-    print("Computing features for evaluation...")
-    start_time = time.time()
-
-    texts = data_loader.dataset.text
-    num_text = len(texts)
-    text_bs = 256
-    text_feats = []
-    text_embeds = []
-    text_atts = []
-    text_inputs = []
-    for i in tqdm(range(0, num_text, text_bs)):
-        text = texts[i : min(num_text, i + text_bs)]
-        text_input = tokenizer(
-            text,
-            padding="max_length",
-            truncation=True,
-            max_length=30,
-            return_tensors="pt",
-        ).to(device)
-        text_inputs.append(text_input)
-        text_output = model.text_encoder(
-            text_input.input_ids, attention_mask=text_input.attention_mask, mode="text"
-        )
-        text_feat = text_output.last_hidden_state
-        text_embed = F.normalize(model.text_proj(text_feat[:, 0, :]))
-        text_embeds.append(text_embed)
-        text_feats.append(text_feat)
-        text_atts.append(text_input.attention_mask)
-    text_embeds = torch.cat(text_embeds, dim=0)
-    text_feats = torch.cat(text_feats, dim=0)
-    text_atts = torch.cat(text_atts, dim=0)
-    text_tokens = torch.cat([_.input_ids for _ in text_inputs], dim=0)
-
-    image_feats = []
-    image_embeds = []
-    for image, img_id in tqdm(data_loader):
-        image = image.to(device)
-        image_feat = model.visual_encoder(image)
-        image_embed = model.vision_proj(image_feat[:, 0, :])
-        image_embed = F.normalize(image_embed, dim=-1)
-
-        image_feats.append(image_feat)
-        image_embeds.append(image_embed)
-
-    image_feats = torch.cat(image_feats, dim=0)
-    image_embeds = torch.cat(image_embeds, dim=0)
-
-    sims_matrix = image_embeds @ text_embeds.t()
-    # sims_matrix = torch.Tensor(np.load('/net/acadia10a/data/zkhan/albef-sims/albef-epoch30-sims.npy')).to(device)
-    score_matrix_i2t = torch.full(
-        (len(data_loader.dataset.image), len(texts)), -100.0
-    ).to(device)
-
-    num_tasks = utils.get_world_size()
-    rank = utils.get_rank()
-    step = sims_matrix.size(0) // num_tasks + 1
-    start = rank * step
-    end = min(sims_matrix.size(0), start + step)
-
-    for i, sims in enumerate(
-        metric_logger.log_every(sims_matrix[start:end], 50, header)
-    ):
-        topk_sim, topk_idx = sims.topk(k=config["k_test"], dim=0)
-
-        encoder_output = image_feats[start + i].repeat(config["k_test"], 1, 1)
-        encoder_att = torch.ones(encoder_output.size()[:-1], dtype=torch.long).to(
-            device
-        )
-        output = model.text_encoder(
-            text_tokens[topk_idx],
-            attention_mask=text_atts[topk_idx],
-            encoder_hidden_states=encoder_output,
-            encoder_attention_mask=encoder_att,
-            return_dict=True,
-            mode="multimodal",
-        )
-        score = model.itm_head(output.last_hidden_state[:, 0, :])[:, 1]
-        score_matrix_i2t[start + i, topk_idx] = score
-
-    sims_matrix = sims_matrix.t()
-    score_matrix_t2i = torch.full(
-        (len(texts), len(data_loader.dataset.image)), -100.0
-    ).to(device)
-
-    step = sims_matrix.size(0) // num_tasks + 1
-    start = rank * step
-    end = min(sims_matrix.size(0), start + step)
-
-    for i, sims in enumerate(
-        metric_logger.log_every(sims_matrix[start:end], 50, header)
-    ):
-
-        topk_sim, topk_idx = sims.topk(k=config["k_test"], dim=0)
-        encoder_output = image_feats[topk_idx]
-        encoder_att = torch.ones(encoder_output.size()[:-1], dtype=torch.long).to(
-            device
-        )
-        output = model.text_encoder(
-            text_tokens[start + i].repeat(config["k_test"], 1),
-            attention_mask=text_atts[start + i].repeat(config["k_test"], 1),
-            encoder_hidden_states=encoder_output,
-            encoder_attention_mask=encoder_att,
-            return_dict=True,
-            mode="multimodal",
-        )
-        score = model.itm_head(output.last_hidden_state[:, 0, :])[:, 1]
-        score_matrix_t2i[start + i, topk_idx] = score
-
-    if args.distributed:
-        dist.barrier()
-        torch.distributed.all_reduce(
-            score_matrix_i2t, op=torch.distributed.ReduceOp.SUM
-        )
-        torch.distributed.all_reduce(
-            score_matrix_t2i, op=torch.distributed.ReduceOp.SUM
-        )
-
-    total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print("Evaluation time {}".format(total_time_str))
-
-    return score_matrix_i2t.cpu().numpy(), score_matrix_t2i.cpu().numpy()
-
-
-@torch.no_grad()
-def itm_eval(scores_i2t, scores_t2i, txt2img, img2txt):
-
-    # Images->Text
-    ranks = np.zeros(scores_i2t.shape[0])
-    for index, score in enumerate(scores_i2t):
-        inds = np.argsort(score)[::-1]
-        # Score
-        rank = 1e20
-        for i in img2txt[index]:
-            tmp = np.where(inds == i)[0][0]
-            if tmp < rank:
-                rank = tmp
-        ranks[index] = rank
-
-    # Compute metrics
-    tr1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
-    tr5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
-    tr10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
-
-    # Text->Images
-    ranks = np.zeros(scores_t2i.shape[0])
-
-    for index, score in enumerate(scores_t2i):
-        inds = np.argsort(score)[::-1]
-        ranks[index] = np.where(inds == txt2img[index])[0][0]
-
-    # Compute metrics
-    ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
-    ir5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
-    ir10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
-
-    tr_mean = (tr1 + tr5 + tr10) / 3
-    ir_mean = (ir1 + ir5 + ir10) / 3
-    r_mean = (tr_mean + ir_mean) / 2
-
-    eval_result = {
-        "txt_r1": tr1,
-        "txt_r5": tr5,
-        "txt_r10": tr10,
-        "txt_r_mean": tr_mean,
-        "img_r1": ir1,
-        "img_r5": ir5,
-        "img_r10": ir10,
-        "img_r_mean": ir_mean,
-        "r_mean": r_mean,
-    }
-    return eval_result
-
 
 def main(args, config):
     utils.init_distributed_mode(args)
@@ -380,16 +211,6 @@ def main(args, config):
         )
         state_dict["visual_encoder_m.pos_embed"] = m_pos_embed_reshaped
 
-        # Drop the MIM head so all keys can be matched. We don't need the MIM head
-        # for inference.
-        # try:
-        #     state_dict.pop('mim_head.weight')
-        #     state_dict.pop('mim_head.bias')
-        #     state_dict.pop('mim_head_m.weight')
-        #     state_dict.pop('mim_head_m.bias')
-        # except KeyError:
-        #     pass
-
         for key in list(state_dict.keys()):
             if "bert" in key:
                 encoder_key = key.replace("bert.", "")
@@ -398,10 +219,6 @@ def main(args, config):
 
         required_keys = model.state_dict().keys()
         state_dict = {k: v for k, v in state_dict.items() if k in required_keys}
-        # for key in model.state_dict().keys():
-        #     if key in state_dict.keys():
-        #         if state_dict[key].shape != model.state_dict()[key].shape:
-        #             del state_dict[key]
         msg = model.load_state_dict(state_dict, strict=True)
 
         print("load checkpoint from %s" % args.checkpoint)
@@ -414,59 +231,13 @@ def main(args, config):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
 
-    arg_opt = utils.AttrDict(config["optimizer"])
-    optimizer = create_optimizer(arg_opt, model)
-    arg_sche = utils.AttrDict(config["schedular"])
-    lr_scheduler, _ = create_scheduler(arg_sche, optimizer)
+    _, _ = search_with_simla(
+        model_without_ddp, test_loader, tokenizer, device, config
+    )
 
-    max_epoch = config["schedular"]["epochs"]
-    warmup_steps = config["schedular"]["warmup_epochs"]
-
-    start_time = time.time()
-    for epoch in range(0, max_epoch):
-
-        _, _ = search_with_simla(
-            model_without_ddp, test_loader, tokenizer, device, config
-        )
-        score_test_i2t, score_test_t2i = evaluation(
-            model_without_ddp, test_loader, tokenizer, device, config
-        )
-
-        if utils.is_main_process():
-
-            test_result = itm_eval(
-                score_test_i2t,
-                score_test_t2i,
-                test_loader.dataset.txt2img,
-                test_loader.dataset.img2txt,
-            )
-            print(test_result)
-
-            if args.evaluate:
-                log_stats = {
-                    **{f"test_{k}": v for k, v in test_result.items()},
-                    "epoch": epoch,
-                }
-                with open(os.path.join(args.output_dir, "log.txt"), "a") as f:
-                    f.write(json.dumps(log_stats) + "\n")
-            else:
-                log_stats = {
-                    **{f"test_{k}": v for k, v in test_result.items()},
-                    "epoch": epoch,
-                }
-                with open(os.path.join(args.output_dir, "log.txt"), "a") as f:
-                    f.write(json.dumps(log_stats) + "\n")
-
-        if args.evaluate:
-            break
-
-        lr_scheduler.step(epoch + warmup_steps + 1)
-        dist.barrier()
-        torch.cuda.empty_cache()
-
-    total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print("Time {}".format(total_time_str))
+    # total_time = time.time() - start_time
+    # total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    # print("Time {}".format(total_time_str))
 
 
 if __name__ == "__main__":
